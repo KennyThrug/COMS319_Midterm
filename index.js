4137ea6fb3f78bad2fec6974eb4cff3aa21f2925
KennyThrug <kennyepstein6@gmail.com>
2023-10-13 11:40:56 -0500

    Basic Main Page
    Made a /very/ page displaying posts, read from data.json

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 
+function createPost(index, data){
+    let parentOfPost = document.getElementById("Post-Contents");
+    let post = document.createElement("div");
+    post.className = "row post-full-body";
+    post.innerHTML = `
+    <header class="PostHeader row">
+        <div class="col-md-4">
+            <p>
+                <strong>
+                    ${data.title}
+                </strong> by ${data.author}<br>
+                Date published: ${data.date_published}
+            </p>
+        </div>
+        <div class="col-md-6 tags">
+            Heres where Tags will be
+        </div>
+        <div class="col-md-2">
+            <p>
+                Date of Post: ${data.date}
+            </p>
+        </div>
+    </header>
+    <div class="col-sm-9 post-text-body">
+        <p>
+            ${data.text}
+        </p>
+    </div>
+    <div class="col-sm-3 post-image-body">
+        <img src="${data.image}" width="50%">
+    </div>
+    `;
+    parentOfPost.appendChild(post);
+}
+function createAllPosts(json_data){
+    for(let i = 0; i < json_data.posts.length;i++){
+       createPost(i,json_data.posts[i]);
+    }
+}
+//This is a function soley in charge of hiding and reshowing the search bar
 function toggleAdvancedSearch(){
     let toggleButton = document.getElementById(`toggleAdvancedSearch`);
     var card = document.getElementById(`AdvancedSearch`);
@@ -7,8 +47,11 @@ function toggleAdvancedSearch(){
         collapsableCard.toggle();
     });
 }
-
+let json;
 //Does the following when the page has fully loaded... Added for extra safety to avoid data races, which I have come across in the past
 window.onload = function() {
     toggleAdvancedSearch();
+    json = fetch("/data.json")
+    .then(response => response.json())
+    .then(posts => createAllPosts(posts));
 }
